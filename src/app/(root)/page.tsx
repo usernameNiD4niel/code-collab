4a54f87d7a5f8338764acc9f975f2036cdfec1c3
Daniel Rey <reyvillanuevadaniel@gmail.com>
2025-02-03 17:19:12 +0800

    added the headers with theme

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,18 @@
+import EditorPanel from "./components/EditorPanel";
+import Header from "./components/Header";
+import OutputPanel from "./components/OutputPanel";
+
+export default function Home() {
+	return (
+		<div className="min-h-screen">
+			<div className="max-w-[1800px] mx-auto p-4">
+				<Header />
+
+				<div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+					<EditorPanel />
+					<OutputPanel />
+				</div>
+			</div>
+		</div>
+	);
+}
